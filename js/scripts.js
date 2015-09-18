1f6dbb2abd5e5393a460b81f1d7877e31b14eb7a
Jeff Austin <jeffaustin81@gmail.com>
2015-09-18 12:08:59 -0700

    Add to form inputs for toppings, add jQuery to display topping values and order when form is submitted

--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,15 @@ function Pizza(pizzaSize, pizzaToppings, pizzaQuantity) {
 Pizza.prototype.pizzaPrice = function() {
     var subtotal = 0;
     var toppings = this.pizzaToppings * 2;
-    if(this.pizzaSize === "Large") {
+    if (this.pizzaSize === "Large") {
         subtotal += 11;
     }
 
-    if(this.pizzaSize === "Medium") {
+    if (this.pizzaSize === "Medium") {
         subtotal += 9;
     }
 
-    if(this.pizzaSize === "Small") {
+    if (this.pizzaSize === "Small") {
         subtotal += 7;
     }
 
@@ -29,10 +29,17 @@ $(document).ready(function() {
     $("form#pizza-order").submit(function(event) {
         event.preventDefault();
 
+        var toppings = $("input#toppings:checked").map(function() {return this.value;}).get().join(', ');
         var pizzaSize = $("select#pizza-size").val();
-        var pizzaToppings = parseInt($(stuff).val());
+        var pizzaToppings = $("input#toppings:checked").length;
         var pizzaQuantity = parseInt($("select#pizza-quantity").val());
 
         var newPizza = new Pizza(pizzaSize, pizzaToppings, pizzaQuantity);
+
+        $(".size").text(newPizza.pizzaSize);
+        $(".toppings").text(toppings);
+        $(".quantity").text(newPizza.pizzaQuantity);
+        $(".price").text(newPizza.pizzaPrice());
+        $("#show-order").show();
     });
 });
