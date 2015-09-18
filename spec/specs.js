1a62d4a6d0baddaf517af47fc5f692c527721689
Jeff Austin <jeffaustin81@gmail.com>
2015-09-18 09:41:54 -0700

    Add test for class Pizza

--- spec/specs.js
+++ spec/specs.js
@@ -1,1 +1,8 @@
-
+describe('Pizza', function() {
+    it("creates a new pizza with the given information", function() {
+            var testPizza = new Pizza("Large", 2, 1);
+            expect(testPizza.pizzaSize).to.equal("Large");
+            expect(testPizza.pizzaToppings).to.equal(2);
+            expect(testPizza.pizzaQuantity).to.equal(1);
+    });
+});
